0729dfd6b6af90d43d112dfc3b4fb8d500473e24
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-20 14:55:33 +0700

    deploy

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
@@ -6,6 +6,8 @@ import { Ionicons } from "@expo/vector-icons";
 import { Image, AsyncStorage } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import * as Sentry from "sentry-expo";
+import Constants from "expo-constants";
 
 import NavController from "./components/NavController";
 import store, { persistor } from "./redux/store";
@@ -25,6 +27,17 @@ const cacheFonts = (fonts: any) =>
 export default function App() {
   const [isReady, setIsReady] = useState(false);
   const handleFinish = () => setIsReady(true);
+  const setSentry = () => {
+    Sentry.init({
+      dsn:
+        "https://ff9f536e81d24e15983fe0dba7c585b2@o282599.ingest.sentry.io/5283707",
+      enableInExpoDevelopment: true,
+      debug: true,
+    });
+    Sentry.setRelease(
+      Constants.manifest.revisionId ? Constants.manifest.revisionId : ""
+    );
+  };
   const loadAssets = async () => {
     await AsyncStorage.clear();
     const images = [
@@ -38,6 +51,9 @@ export default function App() {
     const fontPromises = cacheFonts(fonts);
     return Promise.all([...imagePromises, ...fontPromises]);
   };
+  useEffect(() => {
+    setSentry();
+  }, []);
   return isReady ? (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
